84680d903f219a4a041f40825e86ddf581ec669d
CJuice <CJuice@users.noreply.github.com>
2020-03-31 09:08:11 -0400

    updating from jup notebook dev success code

--- Main.py
+++ Main.py
@@ -9,44 +9,56 @@ GitHub repo for acrgishub module: https://github.com/Esri/hub-py/blob/master/REA
 def main():
 
     # IMPORTS
-    from arcgis.gis import GIS
-    import configparser
-    import os
     from arcgishub import hub
-    import json
+
+    import configparser
+    import datetime
+    import os
 
     # VARIABLES
-    _root_project_path = os.path.dirname(__file__)
-    initiative_id = "2df025de8c074d248175994b2e04d4cf"  # Justin's demo
+    # # # initiative_id = "5a9bc8dfb3e54817ac61fa4d8aa33cc0"  # PROD
+    initiative_id = "5d230c46f10b4c91a60c54e9bca879b6"  # J's demo site cloned to mdimapdatacatalog account
+
+    # TODO: Search for a format that doesn't result in an unusable url
+    date_format = "%y%m%d-%H%M"
+    # backup_text = f"DEVCovidBackup-{datetime.datetime.now().strftime(date_format)}"  # Valid url format
+    backup_text = f"{datetime.datetime.now().strftime(date_format)}-DEVCovidBackup"
 
     # Credentials access and variable creation
-    credentials_file = fr"{_root_project_path}\Credentials\Credentials.cfg"
+    _root_project_folder = os.path.dirname(__file__)
+    credentials_file = fr"{_root_project_folder}\Credentials\Credentials.cfg"
     assert os.path.exists(credentials_file)
     config_parser = configparser.ConfigParser()
     config_parser.read(credentials_file)
-    md_hub_url = config_parser["DEFAULT"]["url"]
-    md_hub_admin = config_parser["DEFAULT"]["login"]
-    md_hub_pwd = config_parser["DEFAULT"]["password"]
 
-    # ASSERTS
-    
-    # FUNCTIONS
+    # md_url = config_parser["DEFAULT"]["url_hub"]
+    md_url = config_parser["DEFAULT"]["url_maps"]
+    md_admin = config_parser["DEFAULT"]["login"]
+    md_pwd = config_parser["DEFAULT"]["password"]
 
-    # CLASSES
+    # Destination folder for clones
+    clone_to_folder = "Hub Clone Automation DEVELOPMENT"
 
     # FUNCTIONALITY
-    # Create a gis connection and get the users in the hub
-    # gis = GIS(url=md_hub_url, username=md_hub_admin, password=md_hub_pwd) # old way??
-    # my_hub = gis.hub
-    print(f"URL: {md_hub_url}")
-    print(f"ADMIN: {md_hub_admin}")
-    print(f"PWD: {md_hub_pwd}")
-    my_hub = hub.Hub(url=md_hub_url, username=md_hub_admin, password=md_hub_pwd)
-    # print(my_hub.enterprise_org_id)
-    try:
-        print(my_hub.initiatives())
-    except json.JSONDecodeError as jde:
-        print(f"y_hub.initiatives() returned {jde}")
+    # Create a connection to the hub using the "new" arcgishub module and explore visibility of target initiative
+    my_hub_arcgishub = hub.Hub(url=md_url, username=md_admin, password=md_pwd)
+
+    # Gets a specific initiative
+    target_initiative_arcgishub = my_hub_arcgishub.initiatives.get(initiative_id)
+    print(f"Initiative title: {target_initiative_arcgishub.title}")
+
+    # This seems to take a few minutes to complete
+    # Clone the initiative and the application (site)
+    cloned_initiative_arcgishub = my_hub_arcgishub.initiatives.clone(target_initiative_arcgishub,
+                                                                     title=f"{backup_text}")
+    cloned_application_arcgishub = my_hub_arcgishub.sites.get(cloned_initiative_arcgishub.site_id)
+
+    # Move initiative item and application item to backup folder
+    # EXCEPTION thrown when item already exists in folder, either because of duplicated names or item already in folder
+    move_initiative_result = cloned_initiative_arcgishub.item.move(folder=clone_to_folder)
+    move_application_result = cloned_application_arcgishub.item.move(folder=clone_to_folder)
+    print(f"Move Initiative response: {move_initiative_result}")
+    print(f"Move Application response: {move_application_result}")
 
 
 if __name__ == "__main__":
